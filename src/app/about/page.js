c4936aa63a8c37e86be1338894c7831a9e0c0100
Uday Raj <udayraj.vadeghar@gmail.com>
2024-06-22 11:28:17 +0530

    minor changes for about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,92 +1,15 @@
 
-
+import Infocards from "@/components/aboutcomponents/infocards";
+import SubscriptionCard from "@/components/aboutcomponents/subscriptionCard";
 
 const About = () => {
   
   return (
-    <div className='mb-10 pb-4'>
-      <div className='lg:grid md:grid grid-cols-3 grid-rows-2 gap-2 p-10 mx-10 place-items-center'>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm'>
-          <div>
-            <img src="https://utfs.io/f/e2cbf413-7c87-4ff9-b14f-682a45f066be-edwipf.png" alt="Our Team" /><br />
-            <button className='border-2 rounded-full p-2 border-red-400 text-red-400'>Company</button>
-            <p className='p-2 font-bold text-lg'>Here at RevenueCat, Employees are Customers Too</p>
-            <p className='p-2 text-md'>At RevenueCat, we prioritize our employees as internal customers, ensuring their satisfaction and growth align with our values.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/872939fb-ac99-48e8-b65c-145651a9d99a-2k9xef.png" className='w-10 h-10' alt="Mark Robert" />
-              <p className='pl-2'><span className='font-bold'>Mark Robert </span><br />March 31, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm'>
-          <div>
-            <img src="https://utfs.io/f/1a36aed2-7a63-4b3f-9f45-6304a81da708-frul5z.png" alt="GoogleIO" /><br />
-            <button className='border-2 rounded-full p-2 text-green-400 border-green-400'>Growth</button>
-            <p className='p-2 font-bold text-lg'>Google I/O 2022</p>
-            <p className='p-2 text-md'>Google I/O 2022: Unveiling groundbreaking technologies and innovations shaping the future of digital experiences.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/c7cacab4-8370-4870-9e77-a8af50dfd2a7-20hvfl.png" className='w-10 h-10' alt="John Abraham" />
-              <p className='pl-2'><span className='font-bold'>John Abraham</span><br />Feb 31, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm'>
-          <div>
-            <img src="https://utfs.io/f/5b4ca6f3-c4b1-44fa-9548-aac44fd95919-3yse07.png" alt="Subclub" /><br />
-            <button className='border-2 rounded-full p-2 text-yellow-400 border-yellow-400'>Company</button>
-            <p className='p-2 font-bold text-lg'>Listen to podcasts in the integrated toolbase</p>
-            <p className='p-2 text-md'>Sharing app building ideas in our Employee App club.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/e331b176-3f28-4068-be09-18cd45bf13e9-1l4ya1.png" className='w-10 h-10' alt="Suizie Madame" />
-              <p className='pl-2'><span className='font-bold'>Suizie Madame</span><br />Feb 13, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm'>
-          <div>
-            <img src="https://utfs.io/f/5d698800-374c-425c-9521-128686f197b1-3wlafm.png" alt="Employee Development" /><br />
-            <button className='border-2 rounded-full p-2 border-blue-600 text-blue-400'>Engineering</button>
-            <p className='p-2 font-bold text-lg'>Freedom and development of our employees</p>
-            <p className='p-2 text-md'>We prioritize the freedom and development of our employees to foster innovation and drive organizational success.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/872939fb-ac99-48e8-b65c-145651a9d99a-2k9xef.png " className='w-10 h-10' alt="Polo Marco" />
-              <p className='pl-2'><span className='font-bold'>Polo Marco</span><br />Jan 24, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm'>
-          <div>
-            <img src="https://utfs.io/f/6d779d01-47d0-4794-bf1a-7d7bba12c7bb-3grnvs.png" alt="Work Life Balance" /><br />
-            <button className='border-2 rounded-full p-2 text-purple-700 border-purple-700'>Company</button>
-            <p className='p-2 font-bold text-lg'>Work Life Balance</p>
-            <p className='p-2 text-md'>"Balance of life": Striving for equilibrium between personal, professional, and social aspects to promote overall well-being and fulfillment.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/872939fb-ac99-48e8-b65c-145651a9d99a-2k9xef.png" className='w-10 h-10' alt="Lopez Lupez" />
-              <p className='pl-2'><span className='font-bold'>Lopez Lupez</span><br />Jan 1, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className='border-2 p-4 m-2 w-7/8 rounded-2xl shadow-sm bg-black text-white'>
-          <div>
-            <img src="https://utfs.io/f/2fcd84f6-14f8-46b4-b696-3d31da8c9e2c-30pv2r.png " alt="Non-Toxic Environment" /><br />
-            <button className='border-2 rounded-full p-2'>Company</button>
-            <p className='p-2 font-bold text-lg'>Non-Toxic Environment</p>
-            <p className='p-2 text-md'>Leaders who possess empathy, active listening skills, and a deep understanding of their team members' strengths, weaknesses, and needs, enabling effective communication.</p>
-            <br />
-            <div className='flex'>
-              <img src="https://utfs.io/f/872939fb-ac99-48e8-b65c-145651a9d99a-2k9xef.png" className='w-10 h-10' alt="Cena Mellow" />
-              <p className='pl-2'><span className='font-bold'>Cena Mellow</span><br />December 31, 2023</p>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-    </div>
+    <>
+    <Infocards />
+    <SubscriptionCard />
+    </>
+    
   );
 };
 
