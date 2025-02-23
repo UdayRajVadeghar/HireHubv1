f45cf2ea8cb03642707d6d1d46ff74fd80ce135d
Uday <udayraj.vadeghar@gmail.com>
2025-02-23 10:04:00 +0000

    revert changes

--- src/components/homeComponents/footer.js
+++ src/components/homeComponents/footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="dark:bg-gray-900 dark:text-white py-4 text-black bg-gray-300 rounded-md">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <p>By Sai Raj Vadeghar</p>
-        <p>Copyright © 2024 sairaj</p>
+        <p>By Uday Raj Vadeghar</p>
+        <p>Copyright © 2024 udayraj</p>
       </div>
     </footer>
   );
